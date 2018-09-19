d53ebb492044b7de24854d8c81f5254a37ecb35d
shantidyah <shanti.dyah@gmail.com>
2018-09-19 18:46:55 +0700

    testing & routing users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const {AddUser} = require('../controllers/users-controller')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post('/', AddUser);
+
 
 module.exports = router;
