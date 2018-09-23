5bab349a91c971ef46b284651954bccdf46563c7
Susan Nio <susan.wen88@gmail.com>
2018-09-23 10:54:55 +0700

    removing logs

--- controllers/recipes_controller.js
+++ controllers/recipes_controller.js
@@ -3,8 +3,6 @@ let axios = require('axios')
 module.exports = {
 	getRecipes: (req, res) => {
         let query = req.query.q
-        console.log('query===>', query) 
-        console.log('type query==>',typeof query) //string
         axios.get(`https://api.edamam.com/search?q=${query}&app_id=7f184b7a&app_key=a66fc2e336697a82fe7c32f769dc3291`)
             .then(({data}) => {
                 let hits = data.hits
@@ -21,7 +19,6 @@ module.exports = {
                     recipes.push(recipe)
                     recipe = {}
                 }
-                console.log('server: from recipes_controllers===>', recipes)
                 res.status(200).json({
                     msg: 'successfully get recipes',
                     recipes
